d520404b0d2a82286364e88910ce49a333018f8c
UttamML <uttamkkp2@gmail.com>
2024-12-31 00:01:51 +0600

    Add files via upload
    conditional stepment

---
+++ conditionalstatement.py
@@ -0,0 +1,17 @@
+
+mark = int(input("Enter number: "))
+
+if mark >= 80:
+    print("You have passed, your grade is A+")
+elif mark >= 75:
+    print("You have passed, your grade is A")
+elif mark >= 70:
+    print("You have passed, your grade is A-")
+elif mark >= 60:
+     print("You have passed, your grade is B")
+elif mark >= 50:
+    print("You have passed, your grade is C")
+elif mark >= 40:
+    print("You have passed, your grade is D")
+else:
+  print("You have failed")
