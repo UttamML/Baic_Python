0e19effd395a97c28aeba0dfbb32a8f67421a198
UttamML <uttamkkp2@gmail.com>
2024-12-30 23:56:44 +0600

    Add files via upload
    area of geomerty

---
+++ area.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+area = a * b
+print("The area is", area)
+area = a * a
+print("The area is", area)
+
+area = 0.5 * a * b
+print("The area is", area)
