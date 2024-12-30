d7dd7655acab2cc375de21dc0e060c8ffb7c01c2
UttamML <uttamkkp2@gmail.com>
2024-12-30 23:58:08 +0600

    Add files via upload
    mathematical operation using assignment operator

---
+++ # Simple Python code to add two variable.py
@@ -0,0 +1,13 @@
+# Simple Python code to add two variables
+a =int(input("Enter first number: "))
+b =int(input("Enter second number: "))
+result =a + b
+print("The result is", result)
+result =a - b
+print("The result is", result)
+result =a % b
+print("The result is", result)
+result =a * b
+print("The result is", result)
+result =a / b
+print("The result is", result)
